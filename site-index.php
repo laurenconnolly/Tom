d2a9155e5b11942a2ab9f117720e7b97c607e345
Robert Erickson <rerickso@uvm.edu>
2023-08-28 16:41:10 -0400

    Create site-index.php

---
+++ site-index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title> CS 008 Lab 1 Design</title>
+    <meta name="author" content="robert erickson">
+    <meta name="description" content="A design for starting Lab 1 of CS 008">
+
+    <style>
+        figure { 
+            float: left;
+            text-align: center;
+            width: 20%;
+        }
+
+        img{
+            max-width: 100%;
+        }
+        </style>
+    </head>
+
+    <body>
+        <figure>
+            <img alt="me, Italy" src="images/me.JPG">
+            <figcaption>Backpacking around Europe duing winter break</figcaption>
+    </figure>
+
+    <h1>CS 008-<em>OL1</em> Fall 2024</h1>
+    <h2>bob erickson - Site Map</h2>
+    <p><a href="admin/code-viewer.php">Admin Page </a><p>
+
+
+<!-- #######################################################################-->
+<section class="lab-layout">
+        <h2 class="header">Lab one - Setting up sitemap.</h2>
+        <section class="public-files">
+            <h3>Public Files</h3>
+            <p><a href="sitemap.php">Site map</a></p>
+            <p><a href="sample.php">Sample Page</a></p>
+        </section>
+
+        <section class="supporting-files">
+            <h3>Supporting files</h3>
+            <p></p>
+        </section>
+
+        <section class="grader-notes">
+            <h3>Notes to grader</h3>
+            <   p></p>
+        </section>
+    </section>
+
+    </body>
+
+    </html>
