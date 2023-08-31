890b0eb84f4572614ab5d5e1f7fb28356c9557f9
Robert Erickson <rerickso@uvm.edu>
2023-08-31 10:09:20 -0400

    Lab 2: Created Home page

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="author" content="Tom">
+    <meta name="description" content="Index file for labs 2-9">
+    <meta name="keywords" content="Lab">
+    <title>Home Page | Bill Smith</title>
+  </head>
+  <body>
+    <main>
+      <h1>Welcome to my home page!</h1>
+
+      <!-- Containers for all content (below) -->
+      <section>
+        <h2>This is section one</h2>
+
+      </section>
+
+      <section>
+        <h2>This is section two</h2>
+
+      </section>
+
+      <section>
+        <h2>This is section three</h2>
+
+      </section>
+
+      <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur fugit deleniti laboriosam id quia explicabo, veritatis numquam dolore quisquam blanditiis adipisci nihil laborum, rerum laudantium eius neque suscipit quis commodi quas labore. Reiciendis molestias nesciunt dolorum sint quia, eaque quos voluptates fugiat. Nisi dicta iste consequuntur quae, ex, reiciendis aperiam sit itaque mollitia illo excepturi veritatis perspiciatis maiores, neque illum ad et impedit, laudantium autem laborum est nulla! Vel harum sunt repellat maxime ipsum laboriosam. Possimus consequatur iure distinctio iusto, esse voluptatibus repudiandae error mollitia quae nulla cumque atque, deleniti corrupti iste, quasi quia id veniam velit doloremque in. Nulla dolorum maxime voluptatibus ullam excepturi labore est hic, rem sint laborum possimus quo ab voluptatum impedit voluptates veritatis animi quae. Veniam maxime assumenda numquam rem totam in voluptatum corporis delectus magnam recusandae, molestiae, praesentium veritatis et, quisquam quaerat sint aspernatur quas cumque impedit pariatur. Aliquam deleniti, perspiciatis fugit enim consequuntur corporis ipsum quod debitis quasi accusamus, sequi alias dicta nihil minima, veniam repellat facilis quibusdam ab odio ea a. Temporibus necessitatibus iste cumque expedita possimus ab molestiae provident at, blanditiis consequatur. Ipsa aliquam magnam, aut. Ad cupiditate ea sint, sunt fugiat, delectus veritatis laudantium possimus aspernatur, pariatur molestias ipsam esse. </p>
+
+      <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur fugit deleniti laboriosam id quia explicabo, veritatis numquam dolore quisquam blanditiis adipisci nihil laborum, rerum laudantium eius neque suscipit quis commodi quas labore. Reiciendis molestias nesciunt dolorum sint quia, eaque quos voluptates fugiat. Nisi dicta iste consequuntur quae, ex, reiciendis aperiam sit itaque mollitia illo excepturi veritatis perspiciatis maiores, neque illum ad et impedit, laudantium autem laborum est nulla! Vel harum sunt repellat maxime ipsum laboriosam. Possimus consequatur iure distinctio iusto, esse voluptatibus repudiandae error mollitia quae nulla cumque atque, deleniti corrupti iste, quasi quia id veniam velit doloremque in. Nulla dolorum maxime voluptatibus ullam excepturi labore est hic, rem sint laborum possimus quo ab voluptatum impedit voluptates veritatis animi quae. Veniam maxime assumenda numquam rem totam in voluptatum corporis delectus magnam recusandae, molestiae, praesentium veritatis et, quisquam quaerat sint aspernatur quas cumque impedit pariatur. Aliquam deleniti, perspiciatis fugit enim consequuntur corporis ipsum quod debitis quasi accusamus, sequi alias dicta nihil minima, veniam repellat facilis quibusdam ab odio ea a. Temporibus necessitatibus iste cumque expedita possimus ab molestiae provident at, blanditiis consequatur. Ipsa aliquam magnam, aut. Ad cupiditate ea sint, sunt fugiat, delectus veritatis laudantium possimus aspernatur, pariatur molestias ipsam esse. </p>
+
+      <ol>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li><cite>Ice cream is good</cite></li>
+      </ol>
+
+      <table>
+        <tr>
+          <th>Names</th>
+          <th>Vanilla</th>
+          <th>Chocolate</th>
+          <th>Strawberry</th>
+        </tr>
+        <tr>
+          <td>Sally</td>
+          <td>Likes</td>
+          <td>Likes</td>
+          <td>Dislikes</td>
+        </tr>
+        <tr>
+          <td>Bob</td>
+          <td>Dislikes</td>
+          <td>Likes</td>
+          <td>Dislikes</td>
+        </tr>
+        <tr>
+          <td>Sarah</td>
+          <td>Likes</td>
+          <td>Dislikes</td>
+          <td>Likes</td>
+        </tr>
+      </table>
+      <a href="https://www.newsweek.com/most-popular-ice-cream-flavors-america-1594000">Click here to find out the most popular ice cream flavors!</a>
+
+      <figure>
+        <img src="https://media.istockphoto.com/id/1244822316/vector/ice-cream-on-a-bowl.jpg?s=612x612&w=0&k=20&c=EJ-SzFyOvJPYN0bf9X1DNCtdZBJ0DTShsTnRn4xonO8=" alt="Very good looking ice cream dish">
+        <figcaption>Look at that delicious ice cream!!</figcaption>
+      </figure>
+
+    </main>
+
+  </body>
+</html>
